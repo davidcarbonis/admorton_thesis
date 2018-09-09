42cba1813c851eca3b594b656ddd3b1a92be223c
Alexander Morton <alexander.david.morton.cern.ch>
2018-09-09 16:56:41 +0100

    Corrected plots

--- figs/tk-upgrade/rootFiles-flatGeometry/Chi2Fitter/phi0ResVsEta_IterationComparison.C
+++ figs/tk-upgrade/rootFiles-flatGeometry/Chi2Fitter/phi0ResVsEta_IterationComparison.C
@@ -2,7 +2,7 @@ void phi0ResVsEta_IterationComparison()
 {
 //=========Macro generated from canvas: Canvas_1_n2/Canvas_1_n2
 //=========  (Sun Sep  9 16:33:46 2018) by ROOT version 6.14/04
-   TCanvas *Canvas_1_n2 = new TCanvas("Canvas_1_n2", "Canvas_1_n2",0,65,1280,925);
+   TCanvas *Canvas_1_n2 = new TCanvas("Canvas_1_n2", "Canvas_1_n2",0,0,1280,925);
    gStyle->SetOptStat(0);
    Canvas_1_n2->Range(-0.4860759,-0.2025316,2.551899,1.063291);;
    Canvas_1_n2->SetFillColor(0);
@@ -320,7 +320,7 @@ void phi0ResVsEta_IterationComparison()
    
    TLegend *leg = new TLegend(0.2,0.47,0.35,0.68,NULL,"brNDC");
    leg->SetBorderSize(0);
-   leg->SetTextSize(0.0390625);
+   leg->SetTextSize(0.04);
    leg->SetLineColor(1);
    leg->SetLineStyle(1);
    leg->SetLineWidth(1);
